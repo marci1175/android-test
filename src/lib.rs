b2cde50918d0d3face6d038f154ab31eaec99cc2
Varga Marcell <128537619+marci1175@users.noreply.github.com>
2024-08-18 19:42:57 +0200

    yes

---
+++ src/lib.rs
@@ -0,0 +1,6 @@
+use std::sync::Once;
+
+#[no_mangle]
+pub fn android_main() {
+    println!("Hello android!");
+}
---
+++ src/main.rs
@@ -0,0 +1,5 @@
+fn main() {
+    android_test::android_main();
+    
+    println!("Hello, world!");
+}
